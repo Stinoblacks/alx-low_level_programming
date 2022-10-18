7a8b6db673be18aa020c6537ad037d36c70dc75c
Augustine <austineokpegwa@gmail.com>
2022-10-18 00:36:12 -0700

    if_else_while in C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-2-print_alphabet.c
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 if no error, non zero if error.
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
