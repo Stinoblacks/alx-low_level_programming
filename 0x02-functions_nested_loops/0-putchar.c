8d868b3ad2627ea9d2ad166e9e102dbbe66e4902
Augustine <austineokpegwa@gmail.com>
2022-10-20 06:22:57 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
-include "main.h"
+#include "main.h"
 /**
-*main- prints _putchar and a new line
-*Return: Always 0
-*/
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -13,6 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	_putchar('\n');
 	return (0);
 }
