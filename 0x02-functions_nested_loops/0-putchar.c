a52dc5b5324aed79cd7f8d7d9231a9f196b81d5b
Augustine <austineokpegwa@gmail.com>
2022-10-20 04:04:37 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,17 @@
-#include <unistd.h> 
- #include "main.h" 
-  
- /** 
-  * main - Check description 
-  * Description: it prints the word _putchar, followed by a new line 
-  * Return: ALways 0 
-  */ 
- int main(void) 
- { 
-  
-         char word[8] = "_putchar"; 
-         int i; 
-  
-         for (i = 0; i < 8; i++) 
-                 _putchar(word[i]); 
-         _putchar('\n'); 
-  
-         return (0); 
-         } 
- 
+include "main.h"
+/**
+*main- prints _putchar and a new line
+*Return: Always 0
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,20 +1,12 @@
-#include <unistd.h> 
-  
-  
-  
+#include "main.h"
+#include <unistd.h>   
+
  /** 
-  * 
-  *  * _putchar - writes the character c to stdout 
-  * 
-  *   * @c: The character to print 
-  * 
-  *    * 
-  * 
-  *     * Return: On success 1. 
-  * 
-  *      * On error, -1 is returned, and errno is set appropriately. 
-  * 
-  *       */ 
+  * _putchar - writes the character c to stdout 
+  * @c: The character to print 
+  * 
+  * Return: On success 1. 
+  */ 
   
  int _putchar(char c) 
   
