57143bcd882e660637e652855b6815a1d052bd51
Stinoblacks <austineokpegwa@gmail.com>
2023-10-10 12:13:57 -0700

    printing the alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - fucntion prints the alphabet 10times
+ * followed by a newline
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
