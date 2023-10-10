27ed5f4f569c1617706753a92309aa8271b48ed7
Stinoblacks <austineokpegwa@gmail.com>
2023-10-10 10:13:53 -0700

    function definition

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard library headers */
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+/* Function prototypes */
+int _putchar(char c);
+
+/* Add prototypes for other functions in your project here */
+
+#endif /* MAIN_H */
+
