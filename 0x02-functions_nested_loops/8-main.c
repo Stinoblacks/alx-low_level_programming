9c9daebc21a6f6e82b4a086edc650f1fa5983ecc
Stinoblacks <austineokpegwa@gmail.com>
2023-10-10 19:07:17 -0700

    100 fibonacci numbers

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+/* 8-main.c */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Your code logic here
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Your code logic here */
+
+    return (0);
+}
+
