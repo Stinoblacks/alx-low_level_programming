4433585d8fdb2ca13266b675c42ea50710ebbea4
Stinoblacks <austineokpegwa@gmail.com>
2023-10-29 05:17:05 -0700

    A program that prints its own name followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[]) {
+    if (argc > 0 && argv[0] != NULL) {
+        printf("%s\n", argv[0]);
+    } else {
+        printf("Program name unavailable\n");
+    }
+
+    return (0);
+}
+
